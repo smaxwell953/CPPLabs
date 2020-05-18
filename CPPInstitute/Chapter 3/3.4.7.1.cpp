e9c00e656514c51a3744a5a8be3ee745a42f23c3
Sara Maxwell <saraann.maxwell@gmail.com>
2020-05-18 14:51:01 -0600

    Add Chapter 3 folder
    Add folder to contain labs from Chapter 3 of C++ Institute and one of the labs

---
+++ CPPInstitute/Chapter 3/3.4.7.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+//3.4.7.1
+
+bool isLeap(int year) {
+
+	if (year %4 != 0){return false;}
+	else if (year%100 != 0){return true;}
+	else if (year%400 != 0){return false;}
+	else{return true;}
+}
+
+int main(void) {
+
+	for(int yr = 1995; yr < 2021; yr++)
+		cout << yr << " -> " << isLeap(yr) << endl;
+	return 0;
+}
