43cd2433a068740dd640956fb6dc99efbd755649
Sara Maxwell <saraann.maxwell@gmail.com>
2020-05-25 00:16:50 -0600

    Calculator

--- CPPInstitute/Chapter 2/2.5.4.1.cpp
+++ CPPInstitute/Chapter 2/2.5.4.1.cpp
@@ -3,9 +3,9 @@ using namespace std;
 //2.5.4.1
 
 int main(void) {
-int type;
 bool exit = false;
 float v1, v2;
+int type;
 
 while(!exit) {
 cout << "MENU:\n"
@@ -13,15 +13,23 @@ cout << "MENU:\n"
 "1 - addition \n" <<
 "2 - subtraction \n" <<
 "3 - multiplication \n" <<
-"4 - division \n ";
+"4 - division \n\n" <<
+"Your choice? ";
 
 cin >> type;
 
+if(type >= 1 && type <= 4) {
+cout << "V1 = ";
+cin >> v1;
+cout << "V2 = ";
+cin >> v2;
+}
+
 switch(type) {
 case 0: exit = true; break;
-case 1: cout << "A + B = " << v1 + v2 << endl; break;
-case 2: cout << "A - B = " << v1 - v2 << endl; break;
-case 3: cout << "A * B = " << v1 * v2 << endl; break;
+case 1: cout << "V1 + V1 = " << v1 + v2 << endl; break;
+case 2: cout << "V1 - V2 = " << v1 - v2 << endl; break;
+case 3: cout << "V1 * V2 = " << v1 * v2 << endl; break;
 case 4: if(v2 == 0.0)
 cout << "Error: Cannot divide by 0." << endl;
 else
@@ -29,6 +37,6 @@ cout << "V1 / V2 = " << v1 / v2 << endl;
 break;
 default: cout << "Bad option: " << type << endl;
 }
-
 }
+return 0;
 }
