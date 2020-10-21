4e70af33c3d4ebec8c7c0bc3e7b90138578fbac6
Sara Maxwell <saraann.maxwell@gmail.com>
2020-10-21 03:57:34 -0600

    Add files via upload

---
+++ CPPInstitute/Chapter 2/2.9.8.3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+//2.9.8.3
+
+int main(void) {
+int vector[] = {1, 7, 3, 8, 3, 7, 1};
+bool ispalindrome = true;
+int n = sizeof(vector) / sizeof(vector[0]);
+
+for(int i = 0; i < n / 2; i++)
+if(vector[i] != vector[n - i - 1]) {
+ispalindrome = false;
+break;
+}
+
+if(ispalindrome)
+cout << "It's a palindrome";
+else
+cout << "It isn't a palindrome";
+cout << endl;
+return 0;
+}
